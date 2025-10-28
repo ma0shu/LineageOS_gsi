5744d5b49dcbfdf249796b2bb5ee9514205102ac
Ma Lingshu <61415309+ma0shu@users.noreply.github.com>
2025-10-28 23:05:09 +0800

    Add IMS product inheritance to treble_arm64_bvN.mk

--- treble_arm64_bvN.mk
+++ treble_arm64_bvN.mk
@@ -1,9 +1,8 @@
 TARGET_GAPPS_ARCH := arm64
 include build/make/target/product/aosp_arm64.mk
 $(call inherit-product, device/phh/treble/base.mk)
-
-
 $(call inherit-product, device/phh/treble/lineage.mk)
+$(call inherit-product, vendor/mediatek/ims/ims.mk)
 
 PRODUCT_NAME := treble_arm64_bvN
 PRODUCT_DEVICE := tdgsi_arm64_ab
